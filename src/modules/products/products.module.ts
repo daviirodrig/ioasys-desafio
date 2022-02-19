d77945bb07ff85ac1dbb9e38050950f8dd5215f2
daviirodrig <30713947+daviirodrig@users.noreply.github.com>
2022-02-19 02:14:48 -0300

    add delete product to module

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -3,13 +3,19 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CreateProductController } from './context/createProduct/createProduct.controller';
 import { CreateProductUseCase } from './context/createProduct/createProduct.useCase';
+import { DeleteProductController } from './context/deleteProduct/deleteProduct.controller';
+import { DeleteProductUseCase } from './context/deleteProduct/deleteProduct.useCase';
 import { UpdateProductController } from './context/updateProduct/updateProduct.controller';
 import { UpdateProductUseCase } from './context/updateProduct/updateProduct.useCase';
 import { ProductRepository } from './repository/product.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([ProductRepository, AdminRepository])],
-  providers: [CreateProductUseCase, UpdateProductUseCase],
-  controllers: [CreateProductController, UpdateProductController],
+  providers: [CreateProductUseCase, UpdateProductUseCase, DeleteProductUseCase],
+  controllers: [
+    CreateProductController,
+    UpdateProductController,
+    DeleteProductController,
+  ],
 })
 export class ProductsModule {}
