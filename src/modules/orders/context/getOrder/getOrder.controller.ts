ad949501dc6cc34b23680ddaed3173d5322ee73a
daviirodrig <30713947+daviirodrig@users.noreply.github.com>
2022-02-21 22:04:55 -0300

    fix(order): only allow owner to get order

--- src/modules/orders/context/getOrder/getOrder.controller.ts
+++ src/modules/orders/context/getOrder/getOrder.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get, Param, Request } from '@nestjs/common';
+import {
+  Controller,
+  ForbiddenException,
+  Get,
+  Param,
+  Request,
+} from '@nestjs/common';
 import { ApiNotFoundResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { UseAuth } from '@shared/decorators/auth.decorator';
 import { Order } from '@shared/entities/order.entity';
@@ -14,8 +20,12 @@ export class GetOrderController {
   @UseAuth()
   @ApiOkResponse({ type: Order, description: 'Success' })
   @ApiNotFoundResponse({ description: 'Order not found' })
-  async getOne(@Param() id: string) {
+  async getOne(@Param() id: string, @Request() req) {
     const order = await this.getOrderUseCase.byId(id);
+
+    if (order.user.id != req.user.id) {
+      throw new ForbiddenException();
+    }
 
     return instanceToInstance(order);
   }
